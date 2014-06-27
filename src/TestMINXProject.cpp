453c952699ae5040f6e082eaf4aa5cdfc0e900a7
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-06-27 01:11:26 -0400

    Implemented Text RenderToTexture for testing

--- src/TestMINXProject.cpp
+++ src/TestMINXProject.cpp
@@ -4,15 +4,19 @@
 #include <MINX/Media/SoundFile.h>
 #include <MINX/Input/GamePad.h>
 #include <MINX/Graphics/Color.h>
+#include <MINX/Graphics/Font.h>
 #include <MINX/Graphics/ShaderFactory.h>
+#include <MINX/Graphics/TextureBatch.h>
 #include <MINX/Input/Keyboard.h>
 
 using namespace MINX_TESTMINXPROJECT;
 using namespace MINX::Graphics;
+Font* myFont;
 Media::SoundFile* clip;
 Input::GamePad* gamePad;
 Input::Keyboard* keyboard;
 bool hasPlayed = false;
+TextureBatch* texBatch;
 TestMINXProject::TestMINXProject()
 {
 	//This is the constructor. Put stuff here that should happen when the Game is created.
@@ -24,7 +28,10 @@ TestMINXProject::TestMINXProject()
 void TestMINXProject::Initialize()
 {
 	//Put stuff here that should happen when the Game is initialized.
+
+
 	Game::Initialize();
+	texBatch = new TextureBatch(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
 }
 
 void TestMINXProject::LoadContent()
@@ -34,6 +41,7 @@ void TestMINXProject::LoadContent()
 	//Sound Stolen Shamelessly from:
 	//http://www.pdsounds.org/sounds/dial_up_connection
 	clip = new Media::SoundFile("../content/beer_splash.wav");
+	myFont = new Font(freeTypeLibrary, "Ubuntu-B.ttf", ShaderFactory::GetInstance()->GetShaderAtIndex(1));
 	gamePad = new Input::GamePad(0,this);
 	keyboard = new Input::Keyboard(this);
 	Game::LoadContent();
@@ -63,8 +71,12 @@ void TestMINXProject::Update(GameTime * gameTime)
 
 void TestMINXProject::Draw(GameTime * gameTime)
 {
-	Graphics::Color backgroundColor = Graphics::Color((gamePad->GetAxis(1).val+1)*128-1, gamePad->GetButton(3).state*255, (gamePad->GetAxis(3).val+1)*128+1);
-	gameWindow->ClearColor(backgroundColor);
+	gameWindow->ClearColor(Color(0,0,0));
+
+	Texture2D* myFontTex = myFont->RenderText("labcdefghiklmnopqrstuvwxyz1234567890", 0, 0, 16, Color(255,255,255,255));
+
+	texBatch->Draw(myFontTex, 10, 10, 1, 1);
+	texBatch->DrawLoadedTextures();
 
 	//Put stuff here to draw your game each frame.
 	Game::Draw(gameTime);
